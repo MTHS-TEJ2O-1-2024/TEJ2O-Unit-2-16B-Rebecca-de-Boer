e630922a6a707d066e459894a29bf06b9ab5a0bb
Rebecca-de-Boer <rebecca.de-boer@mths.ca>
2024-11-13 11:22:28 -0500

    sends text

--- main.ts
+++ main.ts
@@ -5,4 +5,38 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// set up
+radio.setGroup(60)
+basic.showIcon(IconNames.Happy)
+
+// sending 
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Triangle)
+    radio.sendString("Hello, World!")
+    basic.showIcon(IconNames.Happy)
+})
+
+
+// reciving
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
+
+// ending
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// if too close
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    let objectDistance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    if (objectDistance > 10) {
+        basic.showString("you're too close")
+    }
+})
