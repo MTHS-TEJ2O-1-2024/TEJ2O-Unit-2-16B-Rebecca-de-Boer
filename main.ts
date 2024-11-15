8900781592f7839596da779632212415f3026ee6
Rebecca-de-Boer <rebecca.de-boer@mths.ca>
2024-11-15 11:13:19 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Rebecca de Boer
+ * Created on: Nov 2024
+ * This program sends messages
 */
 
 // set up
